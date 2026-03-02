a0ee744bbb1b363bc3e9414f453e304caf1eb99e
Junlang-7 <1448760937@qq.com>
2026-03-02 09:50:00 +0800

    Refactor: move client struct & method to a independent file

---
+++ client/client.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Client struct {
+	ServerIp   string
+	ServerPort int
+	Name       string
+	conn       net.Conn
+}
+
+func NewClient(serverIp string, serverPort int) *Client {
+	client := &Client{
+		ServerIp:   serverIp,
+		ServerPort: serverPort,
+	}
+	conn, err := net.Dial("tcp", net.JoinHostPort(serverIp, fmt.Sprintf("%d", serverPort)))
+	if err != nil {
+		fmt.Println("net.Dial error", err)
+		return nil
+	}
+	client.conn = conn
+	return client
+}
--- client/main.go
+++ client/main.go
@@ -3,29 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net"
 )
-
-type Client struct {
-	ServerIp   string
-	ServerPort int
-	Name       string
-	conn       net.Conn
-}
-
-func NewClient(serverIp string, serverPort int) *Client {
-	client := &Client{
-		ServerIp:   serverIp,
-		ServerPort: serverPort,
-	}
-	conn, err := net.Dial("tcp", net.JoinHostPort(serverIp, fmt.Sprintf("%d", serverPort)))
-	if err != nil {
-		fmt.Println("net.Dial error", err)
-		return nil
-	}
-	client.conn = conn
-	return client
-}
 
 var serverIp string
 var serverPort int
